cd8e789b97d7a43c8805405ebf287053626a08ee
Pumposh Bhat <pumposh@live.com>
2024-10-15 19:17:56 -0400

    test

--- server/api/screenshot.ts
+++ server/api/screenshot.ts
@@ -6,11 +6,19 @@ async function takeScreenshot(
   url: string
 ): Promise<Blob> {
   try {
-    chromium.setHeadlessMode = true;
-    chromium.setGraphicsMode = false;
-
+    chromium.setGraphicsMode = true;
     const browser = await puppeteer.launch({
-      args: chromium.args,
+      args: [
+        ...chromium.args,
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--disable-gpu',
+        '--single-process',
+        '--disable-web-security',
+        '--font-render-hinting=none',
+      ],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(),
       headless: chromium.headless,
